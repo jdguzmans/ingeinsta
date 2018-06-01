b928f3de9b4ac53729a18128052fd9dbdf37b4f7
Juan Guzmán <jdguzmans@hotmail.com>
2018-06-01 12:32:29 -0500

    h

---
+++ back/config.js
@@ -0,0 +1,14 @@
+let config = {}
+
+config.db = {
+  uri: 'mongodb://web:web@ds137740.mlab.com:37740/ingenieria-visible',
+  user: 'web',
+  password: 'web',
+  url: 'ds137740.mlab.com',
+  port: '37740',
+  name: 'ingenieria-visible'
+}
+
+config.awsBucket = 'ingenieria-visible'
+
+module.exports = config
