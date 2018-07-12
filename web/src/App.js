362d5e493dbc3a7e22532cddbf2e7ea4d594a6b5
Juan Guzmán <jdguzmans@hotmail.com>
2018-07-11 21:59:25 -0500

    test

--- web/src/App.js
+++ web/src/App.js
@@ -23,8 +23,7 @@ class App extends Component {
       types: []
     }
 
-    console.log(process.env)
-    this.backURL = process.env.REACT_APP_STAGE === 'dev' ? 'http://localhost:3000' : 'https://api.ingeinsta.com'
+    this.backURL = process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : 'https://api.ingeinsta.com'
 
     this.changeSection = this.changeSection.bind(this)
     this.getTypesAndPoints = this.getTypesAndPoints.bind(this)
